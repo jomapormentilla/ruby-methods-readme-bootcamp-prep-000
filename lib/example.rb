2b1fc7b54168dd6962b79cdf4fff5f925f4bd91d
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 02:06:26 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,9 +1,7 @@
 # Use this space to code along with the readme
 
 def say_hello_world_ten_times
-  
   phrase = "Hello World!"
-  
   puts phrase
   puts phrase
   puts phrase
@@ -14,7 +12,10 @@ def say_hello_world_ten_times
   puts phrase
   puts phrase
   puts phrase
-  
 end
 
-say_hello_world_ten_times
+def greeting
+  puts "Hello World"
+end
+
+greeting
